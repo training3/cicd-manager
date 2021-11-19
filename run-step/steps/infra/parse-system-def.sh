70305239a1c86573f320fe28ae707130b347fb82
Evan A Slatis <eslatis@redhat.com>
2021-11-18 21:54:03 -0600

    testing a poorly written action

--- run-step/steps/infra/parse-system-def.sh
+++ run-step/steps/infra/parse-system-def.sh
@@ -6,4 +6,19 @@ SYSTEM_DEFS_FILE=$(ls ${SYSTEM_DEFS_DIR}/${SYSTEM_NAME}.y*)
 COMPONENT_NAME=$(basename ${WORKING_DIR})
 CODEBASE=$(yq e ".components[] | select(.repo == \"${COMPONENT_NAME}\") | .codebase" ${SYSTEM_DEFS_FILE})
 echo "CODEBASE=${CODEBASE}" >> ${GITHUB_ENV}
+
+
+OVERRIDE_PREFIX=__overrides_
+
+YQ_EXPR=".components[] | select(.repo == \"${COMPONENT_NAME}\") | .overrides | to_entries | .[] | \
+         \"${OVERRIDE_PREFIX}\" + .key + \"=\" + (.value | ..style=\"single\")"
+
+eval $(yq e ${YQ_EXPR} ${SYSTEM_DEFS_FILE})
+unset YQ_EXPR
+
+for VAR in $(set | grep ${OVERRIDE_PREFIX})
+do
+    echo "${VAR}" >> ${GITHUB_ENV}
+done
+
 set +x
