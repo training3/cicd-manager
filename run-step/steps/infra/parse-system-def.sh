e85eaf82a46380c914c1165fbb17e48b31d298ce
Evan A Slatis <eslatis@redhat.com>
2021-11-18 20:31:30 -0600

    testing a poorly written action

--- run-step/steps/infra/parse-system-def.sh
+++ run-step/steps/infra/parse-system-def.sh
@@ -5,5 +5,5 @@ set -x
 SYSTEM_DEFS_FILE=$(ls ${SYSTEM_DEFS_DIR}/${SYSTEM_NAME}.y*)
 COMPONENT_NAME=$(basename ${WORKING_DIR})
 CODEBASE=$(yq e ".components[] | select(.repo == \"${COMPONENT_NAME}\") | .codebase" ${SYSTEM_DEFS_FILE})
-echo ::set-output name=codebase::$(echo ${CODEBASE})
+echo "::set-output name=codebase::$(echo ${CODEBASE})"
 set +x
